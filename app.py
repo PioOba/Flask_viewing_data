72ba9e8e580da40dbfd16255a3a43b6bd705216a
PioOba <piooba5@wp.pl>
2021-10-20 21:33:18 +0200

    Fixed date filtering

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from datetime import datetime
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///charging_statistics.db'
 db = SQLAlchemy(app)
-app.secret_key = {{$ secrets.SECRET_KEY }}
+app.secret_key = 'dsafnkjfdsa'
 
 
 # Create model of the table structure
